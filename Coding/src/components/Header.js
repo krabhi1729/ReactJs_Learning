7f95c401e79f84ca046a0b86d8c8313fc9776156
Abhishek Kumar <krabhi1729@gmail.com>
2023-09-25 22:09:28 +0530

    added search feature,conditional rendering,useEffect

--- Coding/src/components/Header.js
+++ Coding/src/components/Header.js
@@ -1,26 +1,43 @@
 import Logo from "../assets/img/image.png";
+import { useState } from "react";
 
-const Header= () => {
+const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div className="header ">
-      <Title/>
+      <Title />
       <div className="nav-items">
         <ul>
           <li>Home</li>
-           <li>About</li>
-            <li>Contact</li>
-             <li>Cart</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
         </ul>
       </div>
+      {isLoggedIn ? (
+        <button
+          onClick={() => {
+            setIsLoggedIn(false);
+          }}
+        >
+          Logout
+        </button>
+      ) : (
+        <button
+          onClick={() => {
+            setIsLoggedIn(true);
+          }}
+        >
+          Login
+        </button>
+      )}
     </div>
-   
   );
 };
 
-
 const Title = () => (
   <a href="/">
-    <img  className="logo" alt="logo" src={Logo} />
+    <img className="logo" alt="logo" src={Logo} />
   </a>
 );
 export default Header;
